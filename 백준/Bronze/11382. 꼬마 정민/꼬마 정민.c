075400b12cf1a1f1e6e39711acd0a8ec6a7ee97d
GaSap58 <isdnket@naver.com>
2025-04-10 13:43:00 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+    
+    long n1, n2, n3;
+    long sum;
+    
+    scanf("%ld %ld %ld", &n1, &n2, &n3);
+    sum = n1 + n2 + n3;
+    
+    printf("%ld\n", sum);
+    
+    return 0;
+}

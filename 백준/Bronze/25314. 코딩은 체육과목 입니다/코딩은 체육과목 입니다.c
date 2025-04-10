6adaf5ee1b1e5c00b42cc7739af00ebb8b312081
GaSap58 <isdnket@naver.com>
2025-04-10 12:47:49 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    
+    int n;
+    
+    scanf("%d", &n);
+    
+    for (int i = 4; i <= n; i += 4) {
+        printf("long ");
+    }
+    printf("int");
+        
+    return 0;
+}

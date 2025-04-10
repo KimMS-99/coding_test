de9cb46de81224bbda62880cec52939b39d8e893
GaSap58 <isdnket@naver.com>
2025-04-10 14:36:59 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+    
+    int n;
+    
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) {
+        for (int x = n-i; x < n; x++) {
+            printf(" ");
+        }
+        for (int j = n; j > i; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}

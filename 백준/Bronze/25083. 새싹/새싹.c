2bb3499305ef17d0a99ad6b0b97a881ea6d226df
GaSap58 <isdnket@naver.com>
2025-04-14 14:29:01 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+   
+    printf("         ,r'\"%d\n", 7);
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+      
+    return 0;
+}

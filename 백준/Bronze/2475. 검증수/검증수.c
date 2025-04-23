cf5d447fc220d049a1952eadfa45a6fdf6172409
GaSap58 <isdnket@naver.com>
2025-04-24 08:11:13 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    
+    int n[5], sum = 0;
+
+    for (int i = 0; i < 5; i++)
+        scanf("%d", &n[i]);
+        
+    for (int i = 0; i < 5; i++)
+    {
+        sum += n[i] * n[i];
+    }
+
+    printf("%d\n", sum % 10);
+
+    return 0;
+}

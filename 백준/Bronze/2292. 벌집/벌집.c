39e9a697a411540a78120d700998d0e6fa9bd6a3
GaSap58 <isdnket@naver.com>
2025-06-11 08:56:52 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.c
+++ 백준/Bronze/2292. 벌집/벌집.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-
 int main(void)
 {
-
-
-    int n, count = 1, s = 6;
+    int n, s = 6;
     scanf("%d", &n);
-
     for (int i = 7; i < n; i += s)
-    {
         s += 6;
-        count++;
-    }
-    if (n == 1)
-        count--;
-
-    printf("%d\n", count + 1);
-
+    printf("%d", n == 1 ? 1 : s / 6 + 1);
     return 0;
 }

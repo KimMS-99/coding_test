d858917589a62d38324b7394e354ba05e940126a
KimMS-99 <isdnket@naver.com>
2025-08-14 12:55:14 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.c
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.c
@@ -6,42 +6,23 @@ int chess_check(char **chess, int i, int j);
 int main(void)
 {
     int N, M;
-    int i, j;
-    char **chess;
-    goto input;
-start:
-    chess = (char **)malloc(sizeof(char *) * (N + 1));
-    i = 0;
-chess:
-    if (i < N)
+    scanf("%d %d", &N, &M);
+    char **chess = (char **)malloc(sizeof(char *) * (N + 1));
+    for (int i = 0; i < N; i++)
+        chess[i] = (char *)malloc(sizeof(char) * (M + 1));
+
+    for (int i = 0; i < N; i++)
+        scanf("%s", chess[i]);
+
+    int max = 50;
+    for (int i = 0; i <= N - 8; i++)
     {
-        chess[i++] = (char *)malloc(sizeof(char) * (M + 1));
-        goto chess;
-    }
-    i = 0;
-input_chess:
-    if (i < N)
-    {
-        scanf("%s", chess[i++]);
-        goto input_chess;
-    }
-    int max = 64;
-    i = 0;
-chess_check_i:
-    if (i <= N - 8)
-    {
-        j = 0;
-    chess_check_j:
-        if (j <= M - 8)
+        for (int j = 0; j <= M - 8; j++)
         {
             int cnt = chess_check(chess, i, j);
             if (max > cnt)
                 max = cnt;
-            j++;
-            goto chess_check_j;
         }
-        i++;
-        goto chess_check_i;
     }
 
     printf("%d", max);
@@ -51,34 +32,23 @@ chess_check_i:
     free(chess);
 
     return 0;
-
-input:
-    scanf("%d %d", &N, &M);
-    goto start;
 }
 
 int chess_check(char **chess, int i, int j)
 {
     int cnt1 = 0, cnt2 = 0;
-    int a, b;
-    a = i;
-a:
-    if (a < i + 8)
-    {
-        b = j;
-    b:
-        if (b < j + 8)
-        {
-            if (((a + b) % 2 == 0 && chess[a][b] != 'W') || ((a + b) % 2 == 1 && chess[a][b] != 'B'))
-                cnt1++;
-            if (((a + b) % 2 == 0 && chess[a][b] != 'B') || ((a + b) % 2 == 1 && chess[a][b] != 'W'))
-                cnt2++;
-            b++;
-            goto b;
-        }
-        a++;
-        goto a;
-    }
+    char c = chess[i][j];
 
-    return cnt1 < cnt2 ? cnt1 : cnt2;
+    for (int a = i; a < i + 8; a++)
+    {
+        for (int b = j; b < j + 8; b++)
+        {
+            if (((a + b) % 2 == 0 && chess[a][b] != c) || ((a + b) % 2 == 1 && chess[a][b] == c))
+                cnt1++;
+
+            if (((a + b) % 2 == 0 && chess[a][b] == c) || ((a + b) % 2 == 1 && chess[a][b] != c))
+                cnt2++;
+        }
+    }
+    return cnt1 > cnt2 ? cnt2 : cnt1;
 }

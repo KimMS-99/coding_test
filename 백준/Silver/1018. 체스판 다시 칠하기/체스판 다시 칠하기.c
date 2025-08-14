7a3a72409fc39ffc924500fa6d996e111e08856c
KimMS-99 <isdnket@naver.com>
2025-08-14 11:15:46 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.c
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.c
@@ -6,48 +6,79 @@ int chess_check(char **chess, int i, int j);
 int main(void)
 {
     int N, M;
-    scanf("%d %d", &N, &M);
-    char **chess = (char **)malloc(sizeof(char *) * (N + 1));
-    for(int i = 0; i < N; i++)
-        chess[i] = (char *)malloc(sizeof(char) * (M + 1));
-    
-    for(int i = 0; i < N; i++)
-        scanf("%s", chess[i]);
-
-    int max = 50;
-    for(int i = 0; i <= N - 8; i++)
+    int i, j;
+    char **chess;
+    goto input;
+start:
+    chess = (char **)malloc(sizeof(char *) * (N + 1));
+    i = 0;
+chess:
+    if (i < N)
     {
-        for(int j = 0; j <= M - 8; j++)
+        chess[i++] = (char *)malloc(sizeof(char) * (M + 1));
+        goto chess;
+    }
+    i = 0;
+input_chess:
+    if (i < N)
+    {
+        scanf("%s", chess[i++]);
+        goto input_chess;
+    }
+    int max = 64;
+    i = 0;
+chess_check_i:
+    if (i <= N - 8)
+    {
+        j = 0;
+    chess_check_j:
+        if (j <= M - 8)
         {
             int cnt = chess_check(chess, i, j);
-            if(max > cnt)
+            if (max > cnt)
                 max = cnt;
+            j++;
+            goto chess_check_j;
         }
+        i++;
+        goto chess_check_i;
     }
 
     printf("%d", max);
 
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
         free(chess[i]);
     free(chess);
 
     return 0;
+
+input:
+    scanf("%d %d", &N, &M);
+    goto start;
 }
 
 int chess_check(char **chess, int i, int j)
 {
     int cnt1 = 0, cnt2 = 0;
-    char c = chess[i][j];
-
-    for(int a = i; a < i + 8; a++)
+    int a, b;
+    a = i;
+a:
+    if (a < i + 8)
     {
-        for(int b = j; b < j + 8; b++)
+        b = j;
+    b:
+        if (b < j + 8)
         {
-            if(((a + b) % 2 == 0 && chess[a][b] != 'W') || ((a + b) % 2 == 1 && chess[a][b] != 'B'))
+            if (((a + b) % 2 == 0 && chess[a][b] != 'W') || ((a + b) % 2 == 1 && chess[a][b] != 'B'))
                 cnt1++;
-            if(((a + b) % 2 == 0 && chess[a][b] != 'B') || ((a + b) % 2 == 1 && chess[a][b] != 'W'))
+            if (((a + b) % 2 == 0 && chess[a][b] != 'B') || ((a + b) % 2 == 1 && chess[a][b] != 'W'))
                 cnt2++;
+            b++;
+            goto b;
         }
+        a++;
+        goto a;
     }
+
     return cnt1 < cnt2 ? cnt1 : cnt2;
 }
